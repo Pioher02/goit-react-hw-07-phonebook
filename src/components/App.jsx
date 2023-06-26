5315d8af76d79961b006493a3df901e0a2955b26
Pioher02 <dpaola.hernandezr@gmail.com>
2023-06-25 19:00:59 -0500

    loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,21 @@
 import ContactList from './ContactList';
 import ContactForm from './ContactForm';
 import Filter from './Filter';
-// import { deleteContact } from '../redux/contacts/slice';
 import { updateFilter } from '../redux/filter/slice';
 import Notiflix from 'notiflix';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import {fetchContacts, addContact, deleteContact} from "../redux/operations";
+import { fetchContacts, addContact, deleteContact } from '../redux/operations';
+import { Dna } from 'react-loader-spinner';
 
 const App = () => {
   const contacts = useSelector(state => state.contacts.items);
+  const { isLoading, error } = useSelector(state => state.contacts);
   const filter = useSelector(state => state.filter);
   const dispatch = useDispatch();
 
   useEffect(() => {
-     dispatch(fetchContacts());
+    dispatch(fetchContacts());
   }, [dispatch]);
 
   //Guarda contacto nuevo
@@ -62,6 +63,16 @@ const App = () => {
         filter={filter.value}
         deleteContact={handleDeleteContacts}
       />
+      {isLoading && !error && (
+        <Dna
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="dna-loading"
+          wrapperStyle={{}}
+          wrapperClass="dna-wrapper"
+        />
+      )}
     </>
   );
 };
